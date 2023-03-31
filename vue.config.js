7df69ad8c47a83c3f3a04a3fc58e72f600e5afea
oohkumar123 <115493729+oohkumar123@users.noreply.github.com>
2023-03-31 10:00:04 -0700

    changed assets directory

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  assetsDir: "TicTacToeVueJs/assets",
   css: {
     loaderOptions: {
       sass: {
