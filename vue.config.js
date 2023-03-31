89f8f7422927efa3c4f07fe73ca93e6c9495e6be
oohkumar123 <115493729+oohkumar123@users.noreply.github.com>
2023-03-31 10:20:06 -0700

    saved

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  assetsDir: "TicTacToeVueJs/assets",
   css: {
     loaderOptions: {
       sass: {
